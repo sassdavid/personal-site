fd2688fe185a1e0aeddee2c17cadcc1071485453
sassdavid <david.sass14@gmail.com>
2023-02-27 09:09:36 +0100

    hydratefix

--- src/components/Template/ThemeButton.js
+++ src/components/Template/ThemeButton.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import useDarkMode from 'use-dark-mode';
 
 const ThemeBtn = () => {
+  const [mounted, setMounted] = useState(false);
   const darkMode = useDarkMode();
+
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return null;
+
   return (
     <button className="opposite" type="button" onClick={darkMode.toggle}>
       {darkMode.value ? 'Light mode' : 'Dark mode'}
