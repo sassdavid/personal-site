f2102c52647ffcb3cb91f40f2f6fc224adc04f08
sassdavid <david.sass14@gmail.com>
2023-02-27 15:02:24 +0100

    remove hydrate root

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createRoot, hydrateRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
 const StrictApp = () => (
@@ -9,10 +9,6 @@ const StrictApp = () => (
 );
 
 const container = document.getElementById('root');
-let root = createRoot(container);
+const root = createRoot(container);
 
-if (container.hasChildNodes()) {
-  root = hydrateRoot(container, <StrictApp />);
-} else {
-  root.render(<StrictApp />);
-}
+root.render(<StrictApp />);
