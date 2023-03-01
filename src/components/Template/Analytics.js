0b03cfeb5b181b468c035b76a6d539968c4a2dcf
sassdavid <david.sass14@gmail.com>
2023-03-01 12:57:48 +0100

    fix unrecognizable node env variable

--- src/components/Template/Analytics.js
+++ src/components/Template/Analytics.js
@@ -2,9 +2,9 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import ReactGA from 'react-ga4';
 
-const { NODE_ENV, REACT_APP_GA_MEASUREMENT_ID } = process.env;
+const { REACT_APP_ENVIRONMENT, REACT_APP_GA_MEASUREMENT_ID } = process.env;
 
-if (NODE_ENV === 'production') {
+if (REACT_APP_ENVIRONMENT === 'production') {
   ReactGA.initialize(REACT_APP_GA_MEASUREMENT_ID);
 }
 
@@ -12,7 +12,7 @@ const Analytics = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    if (NODE_ENV === 'production') {
+    if (REACT_APP_ENVIRONMENT === 'production') {
       ReactGA.send({ hitType: 'pageview', page: pathname });
     }
   });
