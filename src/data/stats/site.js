b44cf3f638289422bbadfdf98fa336ff3ea4e110
sassdavid <david.sass14@gmail.com>
2023-03-17 22:38:36 +0100

    lines of code

--- src/data/stats/site.js
+++ src/data/stats/site.js
@@ -32,7 +32,7 @@ const data = [
     /* find . | grep ".js" | grep -vE ".min.js|node_modules|.git|.json" |
     xargs -I file cat file | wc -l */
     label: 'Lines of Javascript powering this website',
-    value: '1797',
+    value: '1752',
     link: 'https://github.com/sassdavid/personal-site',
   },
 ];
